b2cda67a17b82291bdf64d2711e1a27c13fb6453
feruzaxabibova99 <feruzaaa1007@gmail.com>
2021-09-25 21:53:33 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,167 @@
+// HTML dan kerakli elementlarni tanlab olish
+let elBreadSelect = document.querySelector(".order__choose-bread-select");
+let elSizeList = document.querySelector(".order__choose-size-list");
+let elMixList = document.querySelector(".order__choose-mix-list");
+let elDetailsList = document.querySelector(".order__choose-details-list");
+let elOrderForm = document.querySelector(".order__form");
+
+//NAtijalarni hisoblagandan keyin uni joylash uchun bosh matnlar
+let elBreadSelectResult = document.querySelector(".bread-select-result");
+let elSizeResult = document.querySelector(".size-result");
+let elMixResult = document.querySelector(".mix-result");
+let elDetailsResult = document.querySelector(".details-result");
+
+//Bu malumotlar keyinchalik beckenddan keladi
+let breadList = ["yupqa", "qalin", "bolka"];
+let sizeList = ['25 cm', '30 cm', '35 cm'];
+let mixList = ["Pomidor", "Kurka goshti", "zaytun", "Kurka go`shti", "Tuzlangan bodiring", "Qazi"];
+let detailsList = ["Achchiq", "Sosiskali"];
+
+//User tanlagan pitsa turlari
+let clientMixList = [];
+let clientDetailsList = [];
+
+elBreadSelect.addEventListener("change", (e) => {
+  elBreadSelectResult.textContent = "";
+  elBreadSelectResult.textContent = elBreadSelect.value;
+});
+
+for (const bread of breadList) {
+  let newBread = document.createElement("option");
+  newBread.setAttribute("value", bread);
+//   newBread.setAttribute("class", "order__choose-bread-select-option");
+  newBread.textContent = bread;
+  elBreadSelect.appendChild(newBread);
+}
+
+for (const size of sizeList) {
+  // element yaratish sizeni berish uchun
+  let newSizeLabel = document.createElement("label");
+  let newSizeInput = document.createElement("input");
+  let newSizeSpan = document.createElement("span");
+
+  // razmerni textcontentiga berish
+  newSizeSpan.textContent = size;
+
+  // elementlarga class qoshib chiqish
+  newSizeSpan.classList.add("order__choose-size-text");
+  newSizeLabel.classList.add("order__choose-size-label");
+  newSizeInput.classList.add("order__choose-size-input", "visually-hidden");
+  newSizeSpan.style.cssText = `padding: 35px;
+                                border-radius: 50%;`;
+
+  //labelga input va spanni qoshish
+  newSizeLabel.appendChild(newSizeInput);
+  newSizeLabel.appendChild(newSizeSpan);
+
+  //labelni htmldagi otasiga qoshib qoyish
+  elSizeList.appendChild(newSizeLabel);
+
+  //inputni atributlarini ozgartirish
+  newSizeInput.setAttribute("type", "radio");
+  newSizeInput.setAttribute("name", "size");
+
+  //input ozgarishiga quloq solish
+  newSizeInput.addEventListener("change", (e) => {
+    elSizeResult.textContent = size;
+  });
+
+  //spanning width va heightlarini jsdan turib ozgartirish
+  newSizeSpan.style.width = `${size * 3.5}px`;
+  newSizeSpan.style.height = `${size * 3.5}px`;
+}
+
+// MIX section
+
+for (const mix of mixList) {
+  // element yaratish mixlani berish uchun
+  let newMixLabel = document.createElement("label");
+  let newMixInput = document.createElement("input");
+  let newMixSpan = document.createElement("span");
+  let newMixInputClone = document.createElement("span");
+
+  // razberni textcontentiga berish
+  newMixSpan.textContent = mix;
+
+  // elementlarga class qoshib chiqish
+  newMixSpan.style.fontSize = '20px';
+  newMixSpan.style.marginLeft = '10px';
+  newMixLabel.style.marginRight = '15px';
+  newMixInput.style.transform = 'scale(2)';
+  newMixInputClone.classList.add("order__choose-mix-clone");
+
+  //inputni atributlarini ozgartirish
+  newMixInput.setAttribute("type", "checkbox");
+  newMixInput.setAttribute("name", "check");
+
+  //labelga input va spanni qoshish
+  newMixLabel.appendChild(newMixInput);
+  newMixLabel.appendChild(newMixInputClone);
+  newMixLabel.appendChild(newMixSpan);
+  console.log(newMixLabel);
+  elMixList.appendChild(newMixLabel);
+
+  newMixInput.addEventListener("change", (e) => {
+    elMixResult.textContent = "";
+    if (!newMixInput.checked) {
+      let indexMix = clientMixList.indexOf(mix);
+      clientMixList.splice(indexMix, 1);
+    } else {
+      clientMixList.push(mix);
+    }
+    for (const cml of clientMixList) {
+      let mixResultText = document.createElement("li");
+      mixResultText;
+      mixResultText.setAttribute("class", "mix-result__item");
+      mixResultText.textContent = cml;
+      elMixResult.appendChild(mixResultText);
+    }
+  });
+}
+for (const detail of detailsList) {
+  // element yaratish mixlani berish uchun
+  let newDetailLabel = document.createElement("label");
+  let newDetailInput = document.createElement("input");
+  let newDetailSpan = document.createElement("span");
+  let newDetailInputClone = document.createElement("span");
+
+  // razberni textcontentiga berish
+  newDetailSpan.textContent = detail;
+
+  // elementlarga class qoshib chiqish
+  newDetailSpan.style.marginLeft = '15px';
+  newDetailSpan.style.fontSize = '20px';
+  newDetailLabel.classList.add("order__choose-detail-label");
+  newDetailInput.style.transform = `scale(2)`;
+//   newDetailInputClone.classList.add("order__choose-detail-clone");
+
+  //inputni atributlarini ozgartirish
+  newDetailInput.setAttribute("type", "checkbox");
+  newDetailInput.setAttribute("name", "check");
+
+  //labelga input va spanni qoshish
+  newDetailLabel.appendChild(newDetailInput);
+  newDetailLabel.appendChild(newDetailInputClone);
+  newDetailLabel.appendChild(newDetailSpan);
+  elDetailsList.appendChild(newDetailLabel);
+
+  newDetailInput.addEventListener("change", (e) => {
+    elDetailsResult.textContent = "";
+    if (!newDetailInput.checked) {
+      let indexDetail = clientDetailsList.indexOf(detail);
+      clientDetailsList.splice(indexDetail, 1);
+    } else {
+      clientDetailsList.push(detail);
+    }
+    for (const cdl of clientDetailsList) {
+      let detailResultText = document.createElement("li");
+    //   detailResultText.setAttribute("class", "details-result__item");
+      detailResultText.textContent = cdl;
+      elDetailsResult.appendChild(detailResultText);
+    }
+  });
+}
+elOrderForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  alert("Sizning buyurtmangiz qabul qilindi");
+});
